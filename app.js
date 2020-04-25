1bac1e212f7ac94ee2a81e2cc029f2a1d73bdead
Joe Innes <joe@joeinn.es>
2020-04-26 00:25:28 +0200

    Main functionality in place

---
+++ app.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+var apos = require('apostrophe')({
+  shortName: 'ada-joe',
+
+  // See lib/modules for basic project-level configuration of our modules
+  // responsible for serving static assets, managing page templates and
+  // configuring user accounts.
+
+  modules: {
+
+    // Apostrophe module configuration
+
+    // Note: most configuration occurs in the respective
+    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+    
+    // However any modules that are not present by default in Apostrophe must at
+    // least have a minimal configuration here: `moduleName: {}`
+
+    // If a template is not found somewhere else, serve it from the top-level
+    // `views/` folder of the project
+
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    'apostrophe-db': {
+      uri: 'mongodb+srv://ada-joe:3McYVliS8uwVpZjV@joessandboxcluster-up1ti.mongodb.net/test?retryWrites=true&w=majority'
+      // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
+      // There is legacy support for host, port, name, user and password options,
+      // but this is not necessary. They can all go in the uri option like this:
+      // mongodb://user:password@host:port/dbname
+    }
+  }
+});
